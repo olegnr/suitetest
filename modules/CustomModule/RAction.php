570e86785e140d1739f9e3f4296bb829dfa1bf0b
olegnr <work.akkr@gmail.com>
2020-03-09 20:53:20 +0500

    inline edit subpanel

--- modules/CustomModule/RAction.php
+++ modules/CustomModule/RAction.php
@@ -6,26 +6,32 @@ $typereq = $_POST['typereq'];
 
 // Формирование списка типов и отраслей (Модуль Контрагенты)
 if($typereq == 'get_select'){
-	$type_select = "<select>";
+	$type_select = array();
 	foreach ($app_list_strings['account_type_dom'] as $key => $val) {         
-		 $type_select .= "<option value='".$key."'>".$val."</option>";
+		$type_select[] = array("type_id"=>$key, "type_name"=>$val);
     }
-	$type_select .= "</select>";
-	
-	$industry_select = "<select>";
+		
+	$industry_select = array();
 	foreach ($app_list_strings['industry_dom'] as $key => $val) {         
-		 $industry_select .= "<option value='".$key."'>".$val."</option>";
+		$industry_select[] = array("industry_id"=>$key, "industry_name"=>$val);
     }
-	$industry_select .= "</select>";
-	
+		
 	$answer = array(
     "type_select" => $type_select,
     "industry_select" => $industry_select,
     );
-	echo json_encode($answer, JSON_UNESCAPED_UNICODE);
-	
+	echo json_encode($answer);
 }
 
+if($typereq == 'update_accounts'){
+	$arrdata = $_POST['itog_pos'];
+	foreach ($arrdata[0] as $key => $value) {
+		$query = "update accounts set account_type = '".$arrdata[1][$key]."', industry = '".$arrdata[2][$key]."' where id = '$value' ";
+		$query_update = $db->query($query);
+	}
+	echo "Данные успешно обновлены";
+
+}
 
 
 
--- modules/CustomModule/js/inline_edit_subpanel.js
+++ modules/CustomModule/js/inline_edit_subpanel.js
@@ -1,7 +1,5 @@
 $(document).ready(function(){
 	
-	var type_select;
-	var industry_select;
 	//Добавляем кнопку "Сохранить изменения"
 	$('<td id = "requesttd">'+
 		'<input style = "" id = "requestin" type="button" class="button" value="Сохранить изменения"/>'+
@@ -10,34 +8,63 @@ $(document).ready(function(){
 	//Получаем значения options для select
 	$.ajax({
  		type: "POST",
-		dataType: "json", // без этого не работает jQuery.each
+		dataType: "json", 
  		data:  {
 			typereq: 'get_select'				
 		},
 		url: 'index.php?module=CustomModule&action=RAction&to_pdf=1',
 		success: function (data){
-			type_select = data.type_select;
-			industry_select = data.industry_select;
+			//заменяем текст в td на select
+			var typevalue;
+			var typeselector;
+			var industryvalue;
+			var industryselector;
+			$('#list_subpanel_custommodule_accounts > table > tbody  > tr').each(function() { 
+				$(this).find('td').each (function(index) {
+					if(index == 2)
+					{
+						typevalue = $(this).html().slice(1);
+						typeselector = $(this);
+						$(this).html('<select></select>');
+						jQuery.each(data.type_select, function(key, val) {
+							if(typevalue == val.type_name)
+							{								
+								typeselector.find('select').append('<option selected="selected" value="' + val.type_id + '">' + val.type_name + '</option>');
+							}
+							else
+							{
+								typeselector.find('select').append('<option value="' + val.type_id + '">' + val.type_name + '</option>');
+							}
+						});
+					}
+					if(index == 3)
+					{
+						industryvalue = $(this).html().slice(1);
+						industryselector = $(this);
+						$(this).html('<select></select>');
+						jQuery.each(data.industry_select, function(key, val) {
+							if(industryvalue == val.industry_name)
+							{								
+								industryselector.find('select').append('<option selected="selected" value="' + val.industry_id + '">' + val.industry_name + '</option>');
+							}
+							else
+							{
+								industryselector.find('select').append('<option value="' + val.industry_id + '">' + val.industry_name + '</option>');
+							}
+						});
+						
+						
+					}
+					
+				});    
+					
+				
+			});
 		}
   	});
 	
 	
-	//заменяем текст в td на select
-	$('#list_subpanel_custommodule_accounts > table > tbody  > tr').each(function() { 
-		$(this).find('td').each (function(index) {
-			if(index == 2)
-			{
-				$(this).html(type_select)
-			}
-			if(index == 3)
-			{
-				$(this).html(industry_select)
-			}
-			
-		});    
-			
-		
-	});
+	
 	
 	//Обработчик кнопки "Сохранить изменения"
 	$('#requesttd').on('click','#requestin',function() {
@@ -53,7 +80,7 @@ $(document).ready(function(){
 			$(this).find('td').each (function(index) {
 				if(index == 1)
 				{
-					ids.push($(this).find('a').html());
+					ids.push($(this).find('a').attr('href').slice(-36));
 				}
 				if(index == 2)
 				{
@@ -72,25 +99,21 @@ $(document).ready(function(){
 		itog_pos=[];
  		itog_pos.length=0;
  		itog_pos=[ids, types, industry];
-		
-		console.log(itog_pos);
-		
-		/*
+				
 		$.ajax({
 	    		type: "POST",
 	    		data:  {
-					param1: 'password',
-					param2: 'requestin',
-					requestid: requestid
+					typereq: 'update_accounts',
+					itog_pos: itog_pos
 					
 				},
 	   		url: 'index.php?module=CustomModule&action=RAction&to_pdf=1',
 	   		success: function (data){
-	   			//alert(data);  
+	   			alert(data);  
 	   			//console.log(data);
 	   		}
 	  	});	
-	  	*/
+	  	
 		
 		return false;	
 	});
